1c06fe2cb2d31e2a30b09c2b0a8497aed1e0c0e2
uriapete <uriarteangelo@gmail.com>
2023-05-16 19:03:51 -0700

    add search form

--- src/pages/AnimIndex.tsx
+++ src/pages/AnimIndex.tsx
@@ -1,10 +1,27 @@
 import { ReactElement } from "react";
 import { AnimeClient,JikanClient,JikanResource,Anime } from "@tutkli/jikan-ts";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./styles/AnimIndex.css"
 
 export default function AnimIndex():ReactElement {
     return(
         <div className="AnimIndex">
-            <p>Anime Index</p>
+            <Container className="search-container">
+                <Form className="search">
+                    <Form.Group className="" as={Row} controlId="search-bar">
+                        <Form.Label column xs="auto" sm="2">
+                            Search: 
+                        </Form.Label>
+                        <Col>
+                            <Form.Control type="text" size="sm"/>
+                        </Col>
+                    </Form.Group>
+                </Form>
+            </Container>
+        
         </div>
     );
 }
