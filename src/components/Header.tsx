98ea89bdcc6e84e141722a09e65c789bd20c2b19
uriapete <uriarteangelo@gmail.com>
2023-05-16 17:36:34 -0700

    add most of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
 import { ReactElement } from "react";
+import { Nav } from "react-bootstrap";
 
 export default function Header():ReactElement {
     return(
-        <header></header>
+        <header>
+            <h1>MoeList!</h1>
+            <Nav variant="pills">
+                <Nav.Item>
+                    <Nav.Link href="/home" >Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/random" >Random</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </header>
     )
 }
