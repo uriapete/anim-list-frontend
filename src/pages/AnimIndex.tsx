0373ba8795e284e60d829dbc491603471b8b58d5
uriapete <uriarteangelo@gmail.com>
2023-05-18 15:55:10 -0700

    display search results on page

--- src/pages/AnimIndex.tsx
+++ src/pages/AnimIndex.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, ReactElement, useState } from "react";
+import { FormEvent, ReactElement, useEffect, useState } from "react";
 import { AnimeClient,JikanResponse,Anime } from "@tutkli/jikan-ts";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -10,44 +10,38 @@ import { NavigateFunction, useLocation, useNavigate } from "react-router";
 
 const animeClient= new AnimeClient();
 
-async function getAnimSearch(searchTerm: string):Promise<JikanResponse<Anime[]>> {
-    const searchData:JikanResponse<Anime[]> = await animeClient.getAnimeSearch({
-        q: searchTerm
-    })
-    return searchData;
-}
 
-interface AnimSearchProps{
-    searchTerm:string;
-}
+// interface AnimSearchProps{
+//     searchTerm:string;
+// }
 
-function AnimSearch(props:AnimSearchProps):ReactElement{
-    const {searchTerm}=props;
-    // const animData:JikanResponse<Anime[]> = await searchAnim(searchTerm);
-    // console.log(animData);
-    return(
-        <div className="anim-search">
+// function AnimSearch(props:AnimSearchProps):ReactElement{
+//     const {searchTerm}=props;
+//     // const animData:JikanResponse<Anime[]> = await searchAnim(searchTerm);
+//     // console.log(animData);
+//     return(
+//         <div className="anim-search">
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
 export default function AnimIndex():ReactElement {
     const s:string= useLocation().search;
     // const [search, setSearch] = useState(new URLSearchParams(s).get("s"));
     const search = new URLSearchParams(s).get("s");
-
-    const [holdSearch,setHoldSearch]=useState("");
     
     const searchStr:string|null = search;
-
+    
+    const [holdSearch,setHoldSearch]=useState("");
+    
     function updateSearchOnInput(e:FormEvent<HTMLInputElement>) {
         setHoldSearch(e.currentTarget.value);
     }
     // console.log(search)
-
+    
     const navigate:NavigateFunction=useNavigate();
-
+    
     function handleSearchSubmit(e:FormEvent) {
         e.preventDefault();
         // console.log("submit!")
@@ -55,6 +49,29 @@ export default function AnimIndex():ReactElement {
         navigate(`/?s=${holdSearch}`)
         // navigate(`/?s=test`)
     }
+
+    const initSearchData:Anime[]=[];
+
+    const [searchData,setSearchData]=useState(initSearchData);
+
+    async function getAnimSearch(searchTerm: string) {
+        const searchData:JikanResponse<Anime[]> = await animeClient.getAnimeSearch({
+            q: searchTerm
+        })
+        setSearchData(searchData.data);
+        // return searchData;
+    }
+
+    useEffect(()=>{
+        try {
+            if(search){
+                getAnimSearch(search);
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    },[])
+
 
     return(
         <div className="AnimIndex">
@@ -73,7 +90,23 @@ export default function AnimIndex():ReactElement {
                     </Form.Group>
                 </Form>
             </Container>
-            {searchStr ? <AnimSearch searchTerm={searchStr}/> : <h1>No search uwu</h1>}
+            {searchStr ? searchData.map((anime,idx)=>{
+                return(
+                    <article key={idx} className="anime-search-result">
+                        <img src={anime.images.jpg.image_url} alt={`${anime.title_english} poster`} className="anime-img" />
+                        <h3 className="anime-title anime-title-en">{anime.title_english}</h3>
+                        <h6 className="anime-title anime-title-jp">{anime.title_japanese}</h6>
+                        <h6 className="studios">Studios: {anime.studios.map((studio,idx)=>{
+                            let studStr:string=studio.name;
+                            if (idx>0){
+                                studStr=" "+studStr;
+                            }
+                            return studStr;
+                        }).join()}</h6>
+                        <p className="anime-synop">{anime.synopsis}</p>
+                    </article>
+                )
+            }) : <h1>No search uwu</h1>}
         </div>
     );
 }
