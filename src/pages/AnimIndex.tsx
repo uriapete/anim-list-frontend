365adbd9dd5c86301f9223ae054d4261daf5f1f3
uriapete <uriarteangelo@gmail.com>
2023-05-18 17:27:38 -0700

    delete dead or commented code, format code, and annotate

--- src/pages/AnimIndex.tsx
+++ src/pages/AnimIndex.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, ReactElement, useEffect, useState } from "react";
-import { AnimeClient,JikanResponse,Anime } from "@tutkli/jikan-ts";
+import { AnimeClient, JikanResponse, Anime } from "@tutkli/jikan-ts";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
@@ -8,84 +8,76 @@ import Button from "react-bootstrap/Button"
 import "./styles/AnimIndex.css"
 import { NavigateFunction, useLocation, useNavigate } from "react-router";
 
-const animeClient= new AnimeClient();
+// initting our client for api calls
+// deals with anime only
+const animeClient = new AnimeClient();
 
+export default function AnimIndex(): ReactElement {
+    // get the current url as an object
+    const loc: string = useLocation().search;
 
-// interface AnimSearchProps{
-//     searchTerm:string;
-// }
+    // get the value of query param "s"
+    const search: string | null = new URLSearchParams(loc).get("s");
 
-// function AnimSearch(props:AnimSearchProps):ReactElement{
-//     const {searchTerm}=props;
-//     // const animData:JikanResponse<Anime[]> = await searchAnim(searchTerm);
-//     // console.log(animData);
-//     return(
-//         <div className="anim-search">
+    // set up navigate function for redirecting
+    const navigate: NavigateFunction = useNavigate();
 
-//         </div>
-//     )
-// }
-
-// interface SearchForm extends formdata
-
-export default function AnimIndex():ReactElement {
-    const s:string= useLocation().search;
-    // const [search, setSearch] = useState(new URLSearchParams(s).get("s"));
-    const search = new URLSearchParams(s).get("s");
-    
-    const searchStr:string|null = search;
-    
-    // const [holdSearch,setHoldSearch]=useState("");
-    
-    // function updateSearchOnInput(e:FormEvent<HTMLInputElement>) {
-    //     setHoldSearch(e.currentTarget.value);
-    // }
-    // console.log(search)
-    
-    const navigate:NavigateFunction=useNavigate();
-    
-    function handleSearchSubmit(e:FormEvent<HTMLFormElement>) {
+    // fn for handling when search query is submitted
+    function handleSearchSubmit(e: FormEvent) {
+        // prevent page from reloading
         e.preventDefault();
-        // console.log("submit!")
+
+        // get form data
         const form = e.target;
-        // console.log(form);
-        const formData:FormData=new FormData(form as HTMLFormElement);
-        // console.log(formData)
-        // return redirect("/uwu");
+
+        // format form data into FormData
+        const formData: FormData = new FormData(form as HTMLFormElement);
+
+        // grab "search" value from formData, redirect to search page using value
         navigate(`/?s=${formData.get("search")}`)
-        // navigate(`/?s=test`)
     }
 
-    const initSearchData:Anime[]=[];
+    // for initting search data state
+    const initSearchData: Anime[] = [];
 
-    const [searchData,setSearchData]=useState(initSearchData);
+    // init search result data state
+    const [searchData, setSearchData] = useState(initSearchData);
 
+    // fn for fetching search data
     async function getAnimSearch(searchTerm: string) {
-        const searchData:JikanResponse<Anime[]> = await animeClient.getAnimeSearch({
+        // using our anime client, fetch a search with our search term
+        const searchData: JikanResponse<Anime[]> = await animeClient.getAnimeSearch({
             q: searchTerm
         })
+
+        // make search data available to this component
         setSearchData(searchData.data);
-        // return searchData;
     }
 
-    useEffect(()=>{
+    // using effect for enabling dynamic loading of search results
+    useEffect(() => {
+        // try/catch, log err if err is caught
         try {
-            if(search){
+            // if there's a search term set, api call for search
+            if (search) {
                 getAnimSearch(search);
             }
         } catch (error) {
             console.log(error)
         }
-    },[search])
+    }, [search])
+    // ^do it again if search changes
 
-
-    return(
-        <div className="AnimIndex">
+    // now here's the actual tsx element
+    return (
+        <div className="AnimIndex" id="AnimIndex">
+            {/* form for search: contains an input "search" and submit button */}
+            {/* using rows, cols, and container from Bootstrap-React for styling */}
             <Container className="search-container">
                 <Form className="search" onSubmit={handleSearchSubmit}>
                     <Form.Group className="" as={Row} controlId="search-bar">
                         <Form.Label column xs="auto" sm="2">
-                            Search: 
+                            Search:
                         </Form.Label>
                         <Col>
                             <Form.Control type="text" size="sm" name="search" />
@@ -96,22 +88,27 @@ export default function AnimIndex():ReactElement {
                     </Form.Group>
                 </Form>
             </Container>
-            {searchStr ? searchData.map((anime,idx)=>{
-                return(
+            {/* now here's the part that'll render the search results */}
+            {/* if we have a search, render the results (using map loop) */}
+            {search ? searchData.map((anime, idx) => {
+                return (
+                    // article which contains anime img, titles, studios
                     <article key={idx} className="anime-search-result">
                         <img src={anime.images.jpg.image_url} alt={`${anime.title_english} poster`} className="anime-img" />
                         <h3 className="anime-title anime-title-en">{anime.title_english}</h3>
                         <h6 className="anime-title anime-title-jp">{anime.title_japanese}</h6>
-                        <h6 className="studios">Studios: {anime.studios.map((studio,idx)=>{
-                            let studStr:string=studio.name;
-                            if (idx>0){
-                                studStr=" "+studStr;
+                        <h6 className="studios">Studios: {anime.studios.map((studio, idx) => {
+                            let studStr: string = studio.name;
+                            if (idx > 0) {
+                                studStr = " " + studStr;
                             }
                             return studStr;
                         }).join()}</h6>
                         <p className="anime-synop">{anime.synopsis}</p>
                     </article>
                 )
+                // else, render case for no search
+                // i plan for having recommended and random section here, that's for later
             }) : <h1>No search uwu</h1>}
         </div>
     );
