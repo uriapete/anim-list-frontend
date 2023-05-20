c0b427960056cf74fb45f3e9359d97522321c13b
uriapete <uriarteangelo@gmail.com>
2023-05-20 11:46:13 -0700

    touch and init SearchResult component and corresponding props interface

---
+++ src/components/SearchResult.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import SearchResultProps from "../interfaces/SearchResultProps";
+
+export default function SearchResult(props:SearchResultProps):ReactElement{
+    return(
+        <h1>test</h1>
+    )
+}
