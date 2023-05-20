4d663e7de9d0085f7d0e2c73646470635d2e3c90
uriapete <uriarteangelo@gmail.com>
2023-05-20 09:45:37 -0700

    format and annotate code

--- src/pages/AnimIndex.tsx
+++ src/pages/AnimIndex.tsx
@@ -91,81 +91,154 @@ export default function AnimIndex(): ReactElement {
             {/* now here's the part that'll render the search results */}
             {/* if we have a search, render the results (using map loop) */}
             {search ? searchData.map((anime, idx) => {
-                let positionClass:string="";
+                // adding class to first and last results
+                // unused classes for now, but could be useful later
+
+                // var for holding the string that holds the class
+                let positionClass: string = "";
+
+                // switch statement: check the idx of the result
                 switch (idx) {
+                    // case for first result
                     case 0:
-                        positionClass+=" first-search-result"
+                        positionClass += " first-search-result"
                         break;
-                
-                    case searchData.length-1:
-                        positionClass+=" last-search-result"
+
+                    // case for last result
+                    case searchData.length - 1:
+                        positionClass += " last-search-result"
                         break;
-                
+
+                    // else, do nothing
                     default:
                         break;
                 }
-                let synop:string="";
-                if(anime.synopsis!==null&&anime.synopsis!==""&&typeof anime.synopsis!=="undefined"){
-                    console.log(anime.synopsis.length)
-                    let breakLength:number=250;
-                    if(window.innerWidth>=500){
+
+                // part for handling synopsis
+
+                // var for synopsis
+                let synop: string = "";
+
+                // if synopsis exists:
+                if (anime.synopsis !== null && anime.synopsis !== "" && typeof anime.synopsis !== "undefined") {
+                    // defining at what amt of chars we'll break the synopsis
+                    // mobile size first:
+                    let breakLength: number = 250;
+
+                    // if desktop, bigger breaklength
+                    if (window.innerWidth >= 500) {
                         breakLength = 700
                     }
-                    if(anime.synopsis.length>breakLength){
-                        let breakCharIdx:number=breakLength+1;
-                        let spaceFound:boolean=false
-                        for (let i = breakLength+1; i > 0; i--) {
+
+                    // if synopsis length exceeds breaklength...
+                    if (anime.synopsis.length > breakLength) {
+                        // var for index of where to break/slice string
+                        let breakCharIdx: number = breakLength + 1;
+
+                        // var for holding whether or not a space " " char was found
+                        let spaceFound: boolean = false
+
+                        // start loop at breaklength, and work back down
+                        for (let i = breakLength + 1; i > 0; i--) {
+                            // defining char
                             const char = anime.synopsis[i];
-                            if(!spaceFound){
-                                if(char===" "){
-                                    spaceFound=true;
+
+                            // if space was not found...
+                            if (!spaceFound) {
+                                // if char is a space, indicate so and go to next iteration
+                                if (char === " ") {
+                                    spaceFound = true;
                                     continue;
-                                }else{
+                                } else {
+                                    // else, go to next iteration
                                     continue;
                                 }
-                            }else{
-                                if(char!==" "){
-                                    breakCharIdx=i+1;
+                            } else {
+                                // this code only runs if a space has been found before
+                                if (char !== " ") {
+                                    // if char is not a space, set the breaking index to the char after it
+                                    // which should be a space and therefore not included in the split string
+                                    breakCharIdx = i + 1;
+
+                                    // then break
                                     break;
-                                }else{
+                                } else {
+                                    // if char is still a space, continue to next iteration
                                     continue;
                                 }
                             }
                         }
-                        synop=anime.synopsis.slice(0,breakCharIdx)+"...";
-                    }else{
-                        synop=anime.synopsis
+                        // set synop var to split string, add "..."
+                        synop = anime.synopsis.slice(0, breakCharIdx) + "...";
+                    } else {
+                        // if synopsis is short and length does not exceed breaklength
+                        // just set it as is
+                        synop = anime.synopsis
                     }
                 }
+
+                // now for the actual display
                 return (
                     // article which contains anime img, titles, studios
-                    <article key={idx} className={"anime-search-result"+positionClass}>
+                    <article key={idx} className={"anime-search-result" + positionClass}>
+
+                        {/* div that contains the picture */}
                         <div className="anime-img search-img result-item">
                             <img src={anime.images.jpg.large_image_url} width={"250vw"} alt={`${anime.title_english} poster`} className="anime-img" />
                         </div>
+
+                        {/* div that contains the text */}
                         <div className="anime-text result-item">
-                            {/* <h3 className="anime-title anime-title-en">{anime.title_english}</h3>
-                            <h6 className="anime-title anime-title-jp">{anime.title_japanese}</h6> */}
+
+                            {/* displaying title */}
+                            {/* if an english title exists: */}
                             {anime.title_english ? (
+                                // display english in big and jp below it in small
                                 <>
                                     <h3 className="anime-title anime-title-en">{anime.title_english}</h3>
                                     <h6 className="anime-title anime-title-jp">{anime.title_japanese}</h6>
                                 </>
+                                // else, if an english title doesn't exist:
                             ) : (
-                                    <h3 className="anime-title anime-title-jp anime-title-jp-big">{anime.title_japanese}</h3> 
+                                // only display jp in big
+                                <h3 className="anime-title anime-title-jp anime-title-jp-big">{anime.title_japanese}</h3>
                             )}
+
+                            {/* displaying studios */}
+                            {/* begin looping thru studios */}
                             <h6 className="studios">Studios: {anime.studios.map((studio, idx) => {
+                                // start map loop, studio (no s, individual) refers to current studio in our arr of studios
+
+                                // setting string var for individual studio
                                 let studStr: string = studio.name;
+
+                                // if our curr studio is not the first on our list...
+                                // add a space " " before the studio name
                                 if (idx > 0) {
                                     studStr = " " + studStr;
                                 }
+
+                                // map gives an array of returned vars, so we'll add our studio strings to the list
                                 return studStr;
+
+                                // and then join the array together for the final display
                             }).join()}</h6>
-                            {synop ? (<p className="anime-synop">{synop}</p>):(<p>No synopsis available.</p>)}
+                            {/* the join() func will add the commas for us, but not add them when there's only one, which is nice */}
+
+                            {/* displaying synopsis */}
+                            {/* 
+                                the synopsis has already been proccessed above
+                                if there is a synopsis, it would've been assigned to synop and cut if it was too long
+                                if there wasn't a synopsis, synop would be "", which is falsy
+                            */}
+                            {/* therefore: if synop is defined, display it */}
+                            {/* if not, display "none available" message */}
+                            {synop ? (<p className="anime-synop">{synop}</p>) : (<p>No synopsis available.</p>)}
                         </div>
                     </article>
                 )
-                // else, render case for no search
+
+                // if there was no search (aka on default home page), render case for no search
                 // i plan for having recommended and random section here, that's for later
             }) : <h1>No search uwu</h1>}
         </div>
