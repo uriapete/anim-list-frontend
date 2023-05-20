27cb2a9788116a502145a2f32e918ca11ff72cca
uriapete <uriarteangelo@gmail.com>
2023-05-20 11:58:49 -0700

    touch and init ListSearchResults component and corresponding props interface

---
+++ src/components/ListSearchResults.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import ListSearchResultsProps from "../interfaces/ListSearchResultsProps";
+
+export default function ListSearchResults(props:ListSearchResultsProps):ReactElement{
+    return(
+        <h1>test</h1>
+    )
+}
