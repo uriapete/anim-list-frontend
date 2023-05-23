930b29b2322539dcab7a311b6b3165ffbf9cb076
uriapete <uriarteangelo@gmail.com>
2023-05-23 16:14:27 -0700

    use malId param

--- src/pages/AnimeShow.tsx
+++ src/pages/AnimeShow.tsx
@@ -1,7 +1,9 @@
 import { ReactElement } from "react";
 import "./styles/AnimeShow.css";
+import { useParams } from "react-router";
 
 export default function AnimeShow():ReactElement{
+    const {malId} = useParams()
     return(
         <h1>Test</h1>
     )
