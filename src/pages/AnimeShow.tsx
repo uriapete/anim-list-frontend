a72b1ea2ef89511207768b2fdd16a3162dd3528f
uriapete <uriarteangelo@gmail.com>
2023-05-23 16:18:59 -0700

    init animeClient

--- src/pages/AnimeShow.tsx
+++ src/pages/AnimeShow.tsx
@@ -1,6 +1,9 @@
 import { ReactElement } from "react";
 import "./styles/AnimeShow.css";
 import { Params, useParams } from "react-router";
+import { AnimeClient,Anime,JikanResponse } from "@tutkli/jikan-ts";
+
+const animeClient:AnimeClient=new AnimeClient();
 
 export default function AnimeShow():ReactElement{
     const params:Params<string> = useParams()
