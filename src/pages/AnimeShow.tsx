ae089a25ae3edec72139d0f73d969aba934bdbb2
uriapete <uriarteangelo@gmail.com>
2023-05-23 16:31:05 -0700

    set up logic for anime loading

--- src/pages/AnimeShow.tsx
+++ src/pages/AnimeShow.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import "./styles/AnimeShow.css";
 import { Params, useParams } from "react-router";
 import { AnimeClient,Anime,JikanResponse } from "@tutkli/jikan-ts";
@@ -8,7 +8,30 @@ const animeClient:AnimeClient=new AnimeClient();
 export default function AnimeShow():ReactElement{
     const params:Params<string> = useParams()
     const malId:number=parseInt(params.malId!);
+
+    const [anime, setAnime] = useState<Anime|null>(null);
+
+    async function getAnimeData() {
+        setAnime((await animeClient.getAnimeById(malId)).data);
+    }
+
+    useEffect(()=>{
+        try {
+            getAnimeData();            
+        } catch (error) {
+            console.log(error);
+        }
+    },[])
+
     return(
-        <h1>Test</h1>
+        <div className="anime-show">
+            {anime?
+            <>
+                <h1>{anime.title_english}</h1>
+            </>
+            :
+            <h1>Loading...</h1>
+            }
+        </div>
     )
 }
