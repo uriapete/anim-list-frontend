d8b0dd14f3628590b73aeb009279645449b52fde
uriapete <uriarteangelo@gmail.com>
2023-05-23 19:52:56 -0700

    center nav content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { ReactElement } from "react";
-import { Nav } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import "./styles/Header.css"
 
 export default function Header():ReactElement {
     return(
         <header>
             <h1>MoeList!</h1>
             <h6>Note: Still under construction!</h6>
-            <Nav variant="pills">
+            <Nav as={"nav"} variant="pills">
                 <Nav.Item>
                     <Nav.Link href="/" >Home</Nav.Link>
                 </Nav.Item>
