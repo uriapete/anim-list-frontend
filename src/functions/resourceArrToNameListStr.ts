a195b5a97dbad78abb2dd11d0b1e1a87a10efd86
uriapete <uriarteangelo@gmail.com>
2023-05-23 18:19:51 -0700

    rename resourceArrToStrList to resourceArrToNameListStr to better reflect what this fn does

---
+++ src/functions/resourceArrToNameListStr.ts
@@ -0,0 +1,5 @@
+import { JikanResource } from "@tutkli/jikan-ts";
+
+export default function resourceArrToNameListStr(resources:JikanResource[]):string{
+    return "";
+}
--- src/functions/resourceArrToStrList.ts
+++
@@ -1,5 +0,0 @@
-import { JikanResource } from "@tutkli/jikan-ts";
-
-export default function resourceArrToStrList(resources:JikanResource[]):string{
-    return "";
-}
