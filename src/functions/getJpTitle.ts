3621572e6db0c9a7703b406aea7467c0fb57714a
uriapete <uriarteangelo@gmail.com>
2023-05-23 18:05:19 -0700

    complete logic for getting jp title

--- src/functions/getJpTitle.ts
+++ src/functions/getJpTitle.ts
@@ -1,5 +1,12 @@
 import { JikanResourceTitle } from "@tutkli/jikan-ts";
 
-export default function getJpTitle(titles:JikanResourceTitle[]){
-
+export default function getJpTitle(titles:JikanResourceTitle[]):string{
+    for (let i = 0; i < titles.length; i++) {
+        const resourceTitle = titles[i];
+        const {type,title} = resourceTitle;
+        if (type==="Japanese"){
+            return title;
+        }
+    }
+    return "";
 }
