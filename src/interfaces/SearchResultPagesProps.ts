d3476f3d04091cfe1860f559d80dbed821ca36c3
uriapete <uriarteangelo@gmail.com>
2023-05-29 14:12:02 -0700

    searchData could be null

--- src/interfaces/SearchResultPagesProps.ts
+++ src/interfaces/SearchResultPagesProps.ts
@@ -1,5 +1,5 @@
 import { Anime, JikanResponse } from "@tutkli/jikan-ts";
 
 export default interface SearchResultPagesProps{
-    searchData:JikanResponse<Anime[]>;
+    searchData:JikanResponse<Anime[]>|null;
 }
