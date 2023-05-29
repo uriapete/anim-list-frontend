7d78e36b5f32ebd4a07620197d6e8a8b0a984b72
uriapete <uriarteangelo@gmail.com>
2023-05-29 13:16:00 -0700

    init SearchResultPages component

---
+++ src/components/SearchResultPages.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+import SearchResultPagesProps from "../interfaces/SearchResultPagesProps";
+
+export default function SearchResultPages(props:SearchResultPagesProps):ReactElement {
+    const{searchData}=props;
+    return(<></>)
+}
