a3fedd871676605d16ad6890a1b89dd372822548
uriapete <uriarteangelo@gmail.com>
2023-05-29 20:23:42 -0700

    create redirect to rand anime

--- src/components/AnimeRandom.tsx
+++ src/components/AnimeRandom.tsx
@@ -1,8 +1,29 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
+import { Anime, JikanResponse } from "@tutkli/jikan-ts";
+import { NavigateFunction, useNavigate } from "react-router";
 
 export default function AnimeRandom():ReactElement {
+    const url: string = "https://api.jikan.moe/v4/random/anime";
+    console.log(url);
+
+    const navigate:NavigateFunction=useNavigate();
+
+    async function fetchRandAnime() {
+        try {
+            const response=await fetch(url);
+            const respData:JikanResponse<Anime>=await response.json();
+            const animeData:Anime=respData.data;
+            navigate(`/anime/${animeData.mal_id}`)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchRandAnime();
+    }, [])
+
     return(
-        <>
-        </>
+        <h1>Loading...</h1>
     )
 }
