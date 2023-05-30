6d3cdbeb2e2b13a4fde3c8b89d61d55f5752fda4
uriapete <uriarteangelo@gmail.com>
2023-05-29 17:12:15 -0700

    fix page buttons reloading page, make it so search results show loading message on new page loading

--- src/components/SearchResultPages.tsx
+++ src/components/SearchResultPages.tsx
@@ -1,9 +1,11 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import SearchResultPagesProps from "../interfaces/SearchResultPagesProps";
 import { useLocation } from "react-router";
 import { Button } from "react-bootstrap";
 import ListSearchResults from "./ListSearchResults";
 import './styles/SearchResultPages.css'
+import { Link } from "react-router-dom";
+import { Anime } from "@tutkli/jikan-ts";
 
 export default function SearchResultPages(props:SearchResultPagesProps):ReactElement {
     const{searchData}=props;
@@ -11,27 +13,79 @@ export default function SearchResultPages(props:SearchResultPagesProps):ReactEle
     const locSearch = useLocation().search
     const s = new URLSearchParams(locSearch).get('s');
     const page = new URLSearchParams(locSearch).get("page")||"1";
+    
+    let numPages:number=1;
+    let pageBtns:ReactElement[]=[];
 
+    // const [animeSearchResults, setAnimeSearchResults] = useState<Anime[] | null>(searchData!==null?searchData.data:null)
+    // // console.log(searchData);
+    
+    // useEffect(() => {
+    // //   first
+    
+    //   const getAnimeSearchResults = async () => {
+    //     try {
+    //         // setAnimeSearchResults(null);
+    //         // console.log(searchData);
+    //         if(searchData!==null){
+    //             setAnimeSearchResults(searchData.data);
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    //   }
+    //   getAnimeSearchResults();
+    // }, [searchData,animeSearchResults])
+    // console.log(animeSearchResults);
+
+    const [resultList, setResultList] = useState<ReactElement>(<></>)
+
+    let animeSearchResults:Anime[]|null=searchData!==null?searchData.data:null;
+    
+    useEffect(() => {
+      const getResultList= () => {
+        try {
+            // console.log(searchData, searchData !== null && searchData.data);
+            if(searchData!==null){
+                setResultList(ListSearchResults({searchData:animeSearchResults}))
+            }
+        } catch (error) {
+            console.log(error)
+        }
+      }
+      getResultList();
+    }, [searchData,animeSearchResults])
+    
+    
     if (searchData === null) {
         return (
             <h1>Loading...</h1>
         )
     }
+        
+        
+    function resetResults() {
+        // console.log("reset")
+        // animeSearchResults=null;
+        setResultList(ListSearchResults({searchData:null}));
+        // console.log(animeSearchResults)
+        // setAnimeSearchResults(null)
+    }
 
     // const hasNextPage=searchData.pagination?.has_next_page;
-    let numPages:number=1;
     if(typeof searchData.pagination?.items !== "undefined"){
         const countPerPage=searchData.pagination!.items!.per_page;
         const total=searchData.pagination!.items!.total;
         numPages=Math.ceil(total/countPerPage);
     }
-    let pageBtns:ReactElement[]=[];
     for (let i = 1; i <= numPages; i++) {
         pageBtns.push((
             <span key={i} className="page-btn-span">
                 {/* <Col key={i} as={"div"} className="page-btn" xs={numPages}> */}
                     {i!==parseInt(page)?
-                    <Button key={i} href={`${path}?s=${s}&page=${i}`}>{i}</Button>
+                    <Link to={`${path}?s=${s}&page=${i}`}>
+                        <Button as={"button"} key={i} onClick={resetResults}>{i}</Button>
+                    </Link>
                     :
                     <Button key={i} disabled>{i}</Button>
                     }
@@ -39,11 +93,14 @@ export default function SearchResultPages(props:SearchResultPagesProps):ReactEle
             </span>
         ))
     }
+    // setAnimeSearchResults(searchData.data);
+    // console.log(animeSearchResults)
     return(
         <div className="search-results-pages">
             {/* <Row className="page-buttons" as={"div"}>{pageBtns}</Row> */}
             <div className="page-buttons page-buttons-top">{pageBtns}</div>
-            <ListSearchResults searchData={searchData} />
+            {/* <ListSearchResults searchData={animeSearchResults} /> */}
+            {resultList}
             <div className="page-buttons page-buttons-bottom">{pageBtns}</div>
         </div>
     )
