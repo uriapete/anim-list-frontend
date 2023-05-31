39b1edc2ed1cb43e9b36403609647b89d922374c
uriapete <uriarteangelo@gmail.com>
2023-05-30 20:31:25 -0700

    formate code

--- src/components/DisplayTopAnime.tsx
+++ src/components/DisplayTopAnime.tsx
@@ -3,10 +3,10 @@ import DisplayTopAnimeProps from "../interfaces/DisplayTopAnimeProps";
 import useTopAnimeData from "../functions/useTopAnimeData";
 import AnimeShortsMap from "./AnimeShortsMap";
 
-export default function DisplayTopAnime(props:DisplayTopAnimeProps):ReactElement{
-    const{filter,limit,page}=props;
-    const{topData}=useTopAnimeData(filter,limit,page);
-    return(
+export default function DisplayTopAnime(props: DisplayTopAnimeProps): ReactElement {
+    const { filter, limit, page } = props;
+    const { topData } = useTopAnimeData(filter, limit, page);
+    return (
         <div className={`display-top-anime list-top-${filter}`}>
             <AnimeShortsMap animeList={topData} />
         </div>
