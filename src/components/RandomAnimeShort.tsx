1127cd5ed06019f42f79625db027d3fe84d81903
uriapete <uriarteangelo@gmail.com>
2023-05-30 19:49:34 -0700

    display random anime

--- src/components/RandomAnimeShort.tsx
+++ src/components/RandomAnimeShort.tsx
@@ -1,7 +1,10 @@
 import { ReactElement } from "react";
+import useRandomAnime from "../functions/useRandomAnime";
+import AnimeShort from "./AnimeShort";
 
 export default function RandomAnimeShort():ReactElement{
+    const randAnimeData=useRandomAnime();
     return(
-        <></>
+        <AnimeShort anime={randAnimeData} additionalClassNames="random-anime-short" />
     )
 }
