b5d46603f9990257534ee37a584228eaf1bdb7cf
uriapete <uriarteangelo@gmail.com>
2023-05-30 20:39:05 -0700

    fix numPages not being preserved

--- src/functions/useTopAnimeData.ts
+++ src/functions/useTopAnimeData.ts
@@ -4,24 +4,27 @@ import { useEffect, useState } from "react";
 import getNumPages from "./getNumPages";
 
 // hook for getting top anime
-export default function useTopAnimeData(filter:TopAnimeFilter,limit:number=5,page:number=1){
-    // init data state
-    const [topData, setTopData] = useState<JikanResponse<Anime[]>|null>(null);
-    let numPages:number=0;
-    
+export default function useTopAnimeData(filter: TopAnimeFilter, limit: number = 5, page: number = 1) {
+    // init data states
+    const [topData, setTopData] = useState<JikanResponse<Anime[]> | null>(null);
+    const [numPages, setNumPages] = useState<number>(0)
+    // let numPages:number=0;
+
     useEffect(() => {
         async function getTopAnimeData() {
-            const fetchTopData: JikanResponse<Anime[]> = await jikanClient.top.getTopAnime({ filter, limit, page })
+            // fetch top anime using supplied filter, limit, and page params
+            const fetchedTopData: JikanResponse<Anime[]> = await jikanClient.top.getTopAnime({ filter, limit, page })
 
-            setTopData(fetchTopData);
+            setTopData(fetchedTopData);
 
-            numPages=getNumPages(fetchTopData);
+            // numPages=getNumPages(fetchedTopData);
+            setNumPages(getNumPages(fetchedTopData))
         }
         try {
             getTopAnimeData();
         } catch (error) {
             console.log(error);
         }
-    }, [filter,limit,page])
-    return{topData,numPages};
+    }, [filter, limit, page])
+    return { topData, numPages };
 }
