51c4602a996d391344581cccdb50832fb6b4551e
uriapete <uriarteangelo@gmail.com>
2023-05-30 19:16:20 -0700

    random anime logic moved to useRandomAnime hook, use useRandomAnime hook instead

--- src/components/AnimeRandom.tsx
+++ src/components/AnimeRandom.tsx
@@ -1,6 +1,7 @@
 import { ReactElement, useEffect } from "react";
 import { Anime, JikanResponse } from "@tutkli/jikan-ts";
 import { NavigateFunction, useNavigate } from "react-router";
+import useRandomAnime from "../functions/useRandomAnime";
 
 export default function AnimeRandom(): ReactElement {
     // there's no getRandomAnime function in jikanClient or animeClient, so we'll do it the old fashioned way
@@ -10,21 +11,27 @@ export default function AnimeRandom(): ReactElement {
     // navigation function - for redirects
     const navigate: NavigateFunction = useNavigate();
 
-    // useEffect - executes as soon as page loads
-    useEffect(() => {
-        // function that fetches random anime
-        async function fetchRandAnime() {
-            try {
-                const response = await fetch(url);
-                const respData: JikanResponse<Anime> = await response.json();
-                const animeData: Anime = respData.data;
-                navigate(`/anime/${animeData.mal_id}`)
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchRandAnime();
-    }, [navigate])
+    // // useEffect - executes as soon as page loads
+    // useEffect(() => {
+    //     // function that fetches random anime
+    //     async function fetchRandAnime() {
+    //         try {
+    //             const response = await fetch(url);
+    //             const respData: JikanResponse<Anime> = await response.json();
+    //             const animeData: Anime = respData.data;
+    //             navigate(`/anime/${animeData.mal_id}`)
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     }
+    //     fetchRandAnime();
+    // }, [navigate])
+
+    const randomAnime = useRandomAnime();
+
+    if (randomAnime!==null){
+        navigate(`/anime/${randomAnime.data.mal_id}`)
+    }
 
     // while fetching, display loading thing
     return (
