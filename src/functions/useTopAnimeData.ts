9f8f92357bcf35ad73c0f972a18c31304a9f9240
uriapete <uriarteangelo@gmail.com>
2023-05-30 18:17:34 -0700

    init params

--- src/functions/useTopAnimeData.ts
+++ src/functions/useTopAnimeData.ts
@@ -1,3 +1,7 @@
-export default function useTopAnimeData(){
+import { TopAnimeFilter } from "@tutkli/jikan-ts";
+import jikanClient from "../clients/jikanClient";
+
+// hook for getting top anime
+export default function useTopAnimeData(filter:TopAnimeFilter,limit:number=5,page:number=1){
     
 }
